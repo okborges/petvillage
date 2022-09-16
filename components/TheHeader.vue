0365e9bb3d384b2c78b0004b2ebcbd1318a0b7a7
Rafael Borges <rafaelborges.av@gmail.com>
2022-09-16 20:54:51 -0300

    header finalizado

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,15 +1,17 @@
 <template>
   <header class="text-white py-3">
-    <div class="container mx-auto flex items-center justify-between">
+    <nav class="container mx-auto items-center justify-between gap-5 lg:flex">
       <img src="~/assets/Pet Village.svg" alt="logo" />
-      <ul class="flex gap-10 text-xl font-semibold">
+      <ul
+        class="flex text-xl font-semibold items-center text-center gap-4 lg:gap-10"
+      >
         <li>Dermatologia e Alergologia</li>
         <li>Banho e tosa</li>
         <li>Hotel</li>
         <li>Creche</li>
         <li>Cursos</li>
       </ul>
-    </div>
+    </nav>
   </header>
 </template>
 
@@ -30,6 +32,28 @@ header {
     background-color: #ff749e;
     border-radius: 30px;
     transition: 0.5s;
+  }
+}
+
+@media screen and (max-width: 1024px) {
+  header {
+    img {
+      margin-left: auto;
+      margin-right: auto;
+      margin-bottom: 10px;
+    }
+
+    ul {
+      flex-wrap: wrap;
+      justify-content: center;
+
+      li {
+        padding: 4px 10px;
+        background-color: #ff749e;
+        border-radius: 4px;
+        font-size: 16px;
+      }
+    }
   }
 }
 </style>
